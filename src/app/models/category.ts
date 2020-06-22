f6d8a2cbc4a2076603a6799d5b36ccd5cf01e7ff
Rodrigo <rmorales@hotmail.com>
2020-06-22 10:49:54 -0300

    Inicio del proyecto configurado

---
+++ src/app/models/category.ts
@@ -0,0 +1,6 @@
+export class category{
+    constructor(
+        public id:number,
+        public name: string
+    ){}
+}
---
+++ src/app/models/user.ts
@@ -0,0 +1,12 @@
+export class user{
+    constructor(
+        public id: number,
+        public name: string,
+        public surename:string,
+        public role: string,
+        public password: string,
+        public description: string,
+        public image:string
+
+    ){}
+}
